8fdfdaaf29313608d47dd7e075d44a33f9145131
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2024-05-18 01:19:06 +0700

    .

--- DiceGlow/Assets/JOELwindows7/AnhaLua/Scripts/AnhaTest.lua
+++ DiceGlow/Assets/JOELwindows7/AnhaLua/Scripts/AnhaTest.lua
@@ -5,19 +5,25 @@
 ]]--
 -- JOELwindows7
 UnityEngine = require("UnityEngine")
+TMP = require("TextMeshPro")
 
 local isSpun = false
 local spounThingy
 local yikYukThingy
+local tmpThingy
 local angle = 0
 local toMoveAt = 0
 local isBacking = false
 local velocity = 5
+local title = 'Halo Lua from JOELwindows7'
+local installSay = ''
+local sayWelcomeHome = ''
 
 -- Start is called before the first frame update
 function Start()
     spounThingy = BoundObjects.Spoun
     yikYukThingy = BoundObjects.IyakYikYuk
+    tmpThingy = BoundObjects.Titler
     print "Hello world!"
 end
 
@@ -39,6 +45,16 @@ function Update()
             isBacking = false
         end
     end
+
+    if InstancesAPI.IsHomeInstance then
+        sayWelcomeHome = "Welcome Home"
+    else
+        sayWelcomeHome = ""
+    end
+
+    installSay = title .. "\n" .. "Ping: " .. InstancesAPI.Ping .. "\n" .. sayWelcomeHome .. "\n"
+    -- tmpThingy.gameObject.TMP_Text.text = installSay
+
     -- spounThingy.transform.Rotate(0,angle,0)
     spounThingy.transform.localRotation = UnityEngine.Quaternion.Euler(UnityEngine.NewVector3(0,angle,0))
     yikYukThingy.transform.localPosition = UnityEngine.NewVector3(.75,toMoveAt,0)
